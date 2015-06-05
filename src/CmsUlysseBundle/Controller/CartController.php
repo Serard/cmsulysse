66d36a9ccd2797aa7135f335a5e08686cd828906
Serard <stephane.erard@free.fr>
2015-06-05 15:25:14 +0200

    cookie cart

--- src/CmsUlysseBundle/Controller/CartController.php
+++ src/CmsUlysseBundle/Controller/CartController.php
@@ -5,13 +5,15 @@ namespace CmsUlysseBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+
 /**
  * @Route("/market")
  */
 class CartController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="market_cart")
      * @Template()
      */
     public function listAction()
@@ -23,36 +25,54 @@ class CartController extends Controller
 
         return array(
                 'products' => $products
-            );    }
+            );
+    }
 
     /**
-     * @Route("/update")
+     * @Route("/update", name="update_cart")
      * @Template()
      */
     public function updateAction()
     {
         return array(
                 // ...
-            );    }
+            );
+    }
 
     /**
-     * @Route("/delete")
+     * @Route("/delete", name="delete_cart")
      * @Template()
      */
     public function deleteAction()
     {
         return array(
                 // ...
-            );    }
+            );
+    }
 
     /**
-     * @Route("/cart")
+     * @Route("/cart", name="user_cart")
      * @Template()
      */
-    public function cartAction()
+    public function cartAction(Request $request)
     {
+        $repo = $this->getDoctrine()->getManager()->getRepository('CmsUlysseBundle:UserProduct');
+        $products = json_decode($request->cookies->get('cart'));
+
+        if ($products) {
+            foreach ($products as $product) {
+                $dbproduct = $repo->find($product->id);
+                $product->name        = $dbproduct->getProduct()->getName();
+                $product->description = $dbproduct->getProduct()->getDescription();
+                $product->price       = $dbproduct->getPrice();
+                $product->stock       = $dbproduct->getQty();
+                $product->seller      = $dbproduct->getUser()->getFirstname().' '.$dbproduct->getUser()->getLastName();
+            }
+        }
+
         return array(
-                // ...
-            );    }
+                'products' => $products
+            );
+    }
 
 }
