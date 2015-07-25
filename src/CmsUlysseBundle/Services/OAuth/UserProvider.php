40834c7c367a0e9f77d4ebf4a428cbe78b28ab08
Pascal Naviere <pascalou@gmail.com>
2015-07-25 18:14:41 +0200

    modif en cours 2

--- src/CmsUlysseBundle/Services/OAuth/UserProvider.php
+++ src/CmsUlysseBundle/Services/OAuth/UserProvider.php
@@ -1,98 +1,210 @@
 <?php
-
 namespace CmsUlysseBundle\Services\OAuth;
 
-use FOS\UserBundle\Model\UserInterface as FOSUserInterface;
-
 use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
-use HWI\Bundle\OAuthBundle\Security\Core\User\FOSUBUserProvider;
-
-use CmsUlysseBundle\Entity\User;
-
-use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use HWI\Bundle\OAuthBundle\Security\Core\User\FOSUBUserProvider as BaseClass;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-/**
- * Loading and ad-hoc creation of a user by an OAuth sign-in provider account.
- *
- * @author Fabian Kiss <fabian.kiss@ymc.ch>
- */
-class UserProvider extends FOSUBUserProvider
+class UserProvider extends BaseClass
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function loadUserByOAuthUserResponse(UserResponseInterface $response)
-    {
-        try {
-            return parent::loadUserByOAuthUserResponse($response);
-        } catch (UsernameNotFoundException $e) {
-            if (null === $user = $this->userManager->findUserByEmail($response->getEmail())) {
-                return $this->createUserByOAuthUserResponse($response);
-            }
-
-            return $this->updateUserByOAuthUserResponse($user, $response);
-        }
-    }
 
     /**
      * {@inheritDoc}
      */
     public function connect(UserInterface $user, UserResponseInterface $response)
     {
-        $providerName = $response->getResourceOwner()->getName();
-        $uniqueId = $response->getUsername();
-        $user->addOAuthAccount($providerName, $uniqueId);
+
+        $property = $this->getProperty($response);
+        $username = $response->getUsername();
+
+        //on connect - get the access token and the user ID
+        $service = $response->getResourceOwner()->getName();
+
+        $setter       = 'set' . ucfirst($service);
+        $setter_id    = $setter . 'Id';
+        $setter_token = $setter . 'AccessToken';
+
+        //we "disconnect" previously connected users
+        if (null !== $previousUser = $this->userManager->findUserBy(array($property => $username))) {
+            $previousUser->$setter_id(null);
+            $previousUser->$setter_token(null);
+            $this->userManager->updateUser($previousUser);
+        }
+
+        //we connect current user
+        $user->$setter_id($username);
+        $user->$setter_token($response->getAccessToken());
 
         $this->userManager->updateUser($user);
     }
 
     /**
-     * Ad-hoc creation of user
-     *
-     * @param UserResponseInterface $response
-     *
-     * @return User
+     * {@inheritdoc}
      */
-    protected function createUserByOAuthUserResponse(UserResponseInterface $response)
+    public function loadUserByOAuthUserResponse(UserResponseInterface $response)
     {
-        $user = $this->userManager->createUser();
-        $this->updateUserByOAuthUserResponse($user, $response);
 
-        // set default values taken from OAuth sign-in provider account
-        if (null !== $email = $response->getEmail()) {
-            $user->setEmail($email);
+        $paths = $response->getPaths();
+        $property     = $this->getProperty($response);
+        $responseData = $response->getResponse();
+
+        # var_dump($responseData);
+        # die;
+
+        if($property == 'doyobuzzId'){
+            $responseData = $responseData['user'];
         }
 
-        if (null === $this->userManager->findUserByUsername($response->getNickname())) {
-            $user->setUsername($response->getNickname());
+        $username     = $responseData[$paths['identifier']];
+
+        $data = array(
+            'username'   => isset($responseData[$paths['email']])? $responseData[$paths['email']] : $username,
+            'first_name' => isset($responseData[$paths['nickname']])? $responseData[$paths['nickname']] : $username,
+            'last_name'  => isset($responseData[$paths['realname']])? $responseData[$paths['realname']] : $username,
+            'email'      => isset($responseData[$paths['email']])? $responseData[$paths['email']] : $username,
+        );
+
+
+        switch ($property) {
+
+            case 'linkedinId':
+                list($last_name, $first_name) = split(' ', $responseData['formattedName']);
+
+                if (isset($responseData['emailAddress'])) {
+                    $data['username'] = $responseData['emailAddress'];
+                }
+
+                if ($last_name) {
+                    $data['last_name'] = $last_name;
+                }
+
+                if ($first_name) {
+                    $data['first_name'] = $first_name;
+                }
+
+                if (isset($responseData['emailAddress'])) {
+                    $data['email'] = $responseData['emailAddress'];
+                }
+
+                break;
+
+            case 'googleId':
+
+                if (isset($responseData['email'])) {
+                    $data['username'] = $responseData['email'];
+                }
+
+                if (isset($responseData['given_name'])) {
+                    $data['last_name'] = $responseData['given_name'];
+                }
+
+                if (isset($responseData['family_name'])) {
+                    $data['first_name'] = $responseData['family_name'];
+                }
+
+                if (isset($responseData['email'])) {
+                    $data['email'] = $responseData['email'];
+                }
+
+                break;
+
+            case 'facebookId':
+
+                if (isset($responseData['email'])) {
+                    $data['username'] = $responseData['email'];
+                }
+
+                if (isset($responseData['last_name'])) {
+                    $data['last_name'] = $responseData['last_name'];
+                }
+
+                if (isset($responseData['first_name'])) {
+                    $data['first_name'] = $responseData['first_name'];
+                }
+
+                if (isset($responseData['email'])) {
+                    $data['email'] = $responseData['email'];
+                }
+
+                break;
+
+            case 'doyobuzzId':
+
+                if (isset($responseData['email'])) {
+                    $data['username'] = $responseData['email'];
+                    $data['email']    = $responseData['email'];
+                }
+
+                if (isset($responseData['lastname'])) {
+                    $data['last_name'] = $responseData['lastname'];
+                }
+
+                if (isset($responseData['firstname'])) {
+                    $data['first_name'] = $responseData['firstname'];
+                }
+
+                break;
         }
 
-        $user->setEnabled(true);
+       # var_dump($data);
+       # var_dump($property);
+       # var_dump($username);
+       # die;
+        //when the user is registrating
+
+        $user      = $this->userManager->findUserBy(array($property => $username));
+        $userEmail = $this->userManager->findUserBy(array('username' => $data['username']));
+
+        #var_dump($user);
+        #var_dump($userEmail);
+        #die;
+
+        if (null != $userEmail && null === $user) {
+
+            $service      = $response->getResourceOwner()->getName();
+            $setter       = 'set' . ucfirst($service);
+            $setter_id    = $setter . 'Id';
+            $setter_token = $setter . 'AccessToken';
+            $userEmail->$setter_id($username);
+            $userEmail->$setter_token($response->getAccessToken());
+            $userEmail->setFBGOData($data);
+            $userEmail->setEnabled(true);
+            $this->userManager->updateUser($userEmail);
+
+            return $userEmail;
+
+        } elseif (null === $user) {
+            $service      = $response->getResourceOwner()->getName();
+            $setter       = 'set' . ucfirst($service);
+            $setter_id    = $setter . 'Id';
+            $setter_token = $setter . 'AccessToken';
+            // create new user here
+            $user = $this->userManager->createUser();
+            $user->$setter_id($username);
+            $user->$setter_token($response->getAccessToken());
+            //I have set all requested data with the user's username
+            //modify here with relevant data
+            $user->setFBGOData($data);
+            $user->setPassword(sha1($username));
+            $user->setEnabled(true);
+            $this->userManager->updateUser($user);
+
+            return $user;
+
+        }
+
+        if($property != 'doyobuzzId') {
+            //if user exists - go with the HWIOAuth way
+            $user = parent::loadUserByOAuthUserResponse($response);
+        }
+
+        $serviceName = $response->getResourceOwner()->getName();
+        $setter      = 'set' . ucfirst($serviceName) . 'AccessToken';
+
+        //update access token
+        $user->$setter($response->getAccessToken());
 
         return $user;
     }
 
-    /**
-     * Attach OAuth sign-in provider account to existing user
-     *
-     * @param FOSUserInterface      $user
-     * @param UserResponseInterface $response
-     *
-     * @return FOSUserInterface
-     */
-    protected function updateUserByOAuthUserResponse(FOSUserInterface $user, UserResponseInterface $response)
-    {
-        $providerName = $response->getResourceOwner()->getName();
-        $providerNameSetter = 'set'.ucfirst($providerName).'Id';
-        $user->$providerNameSetter($response->getUsername());
-
-        if(!$user->getPassword()) {
-            // generate unique token
-            $secret = md5(uniqid(rand(), true));
-            $user->setPassword($secret);
-        }
-
-        return $user;
-    }
 }
